ea9a31d603957ef12720c6d9b4e9505cbf3a1fad
jessicazu <jessicazu926@gmail.com>
2021-10-09 15:55:48 +0900

    fix tailwind setting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    purge: ['./pages/**/*.tsx', './components/**/*.tsx'], // tailwindをどの階層にあるtsファイルに適用するか
     tailwindcss: {},
     autoprefixer: {},
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'], // tailwindをどの階層にあるtsファイルに適用するか
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
